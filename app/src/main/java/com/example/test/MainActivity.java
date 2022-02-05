be75a52543458aa4a5a0de6a93c062aa2541a7ac
amp-patel <amppatel209uk@gmail.com>
2022-02-05 23:50:20 +0000

    Add Version 1.1

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,249 @@
+package com.example.test;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class MainActivity extends AppCompatActivity {
+
+    private static SeekBar seek_bar;
+    private static TextView text_View;
+
+    private CameraManager mCameraManager;
+    private String mCameraId;
+
+    public int progress_value = 9;
+
+    RelativeLayout relly1;
+
+    private static final int POLL_INTERVAL = 50;
+
+    /** running state **/
+    private boolean mRunning = false;
+
+    /** config state **/
+    private int mThreshold;
+
+    int RECORD_AUDIO = 0;
+    private PowerManager.WakeLock mWakeLock;
+
+    private Handler mHandler = new Handler();
+
+    /* References to view elements */
+    private TextView mStatusView,tv_noice;
+
+    /* sound data source */
+    private DetectNoise mSensor;
+    ProgressBar bar;
+    /****************** Define runnable thread again and again detect noise *********/
+
+    private Runnable mSleepTask = new Runnable() {
+        public void run() {
+            //Log.i("Noise", "runnable mSleepTask");
+            start();
+        }
+    };
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            relly1.setVisibility(View.VISIBLE);
+        }
+    };
+
+    // Create runnable thread to Monitor Voice
+    private Runnable mPollTask = new Runnable() {
+        public void run() {
+            double amp = mSensor.getAmplitude();
+            //Log.i("Noise", "runnable mPollTask");
+            updateDisplay("Monitoring Voice...", amp);
+
+            if ((amp > mThreshold)) {
+                callForHelp(amp);
+                //Log.i("Noise", "==== onCreate ===");
+            }
+            else
+            {
+                switchFlashLight(false);
+            }
+            // Runnable(mPollTask) will again execute after POLL_INTERVAL
+            mHandler.postDelayed(mPollTask, POLL_INTERVAL);
+        }
+    };
+    /** Called when the activity is first created. */
+    @SuppressLint("InvalidWakeLockTag")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Defined SoundLevelView in main.xml file
+        setContentView(R.layout.activity_main);
+
+        seekbarr();
+
+        relly1 = (RelativeLayout) findViewById(R.id.relly1);
+
+        handler.postDelayed(runnable, 2000);
+
+        boolean isFlashAvailable = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        }
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                mCameraId = mCameraManager.getCameraIdList()[0];
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        mStatusView = (TextView) findViewById(R.id.status);
+        tv_noice=(TextView)findViewById(R.id.tv_noice);
+        bar=(ProgressBar)findViewById(R.id.progressBar1);
+        // Used to record voice
+        mSensor = new DetectNoise();
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "NoiseAlert");
+
+
+    }
+
+    public void seekbarr()
+    {
+        seek_bar = (SeekBar)findViewById(R.id.seekbar);
+        text_View = (TextView)findViewById(R.id.seektext);
+        text_View.setText(seek_bar.getProgress() + " dB");
+
+        seek_bar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                        progress_value = progress;
+                        text_View.setText(progress + " dB");
+                        initializeApplicationConstants();
+
+
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                        text_View.setText(progress_value + " dB");
+
+                    }
+                }
+        );
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //Log.i("Noise", "==== onResume ===");
+
+        initializeApplicationConstants();
+        if (!mRunning) {
+            mRunning = true;
+            start();
+        }
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        // Log.i("Noise", "==== onStop ===");
+        //Stop noise monitoring
+        stop();
+    }
+    private void start() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
+                    RECORD_AUDIO);
+        }
+
+        //Log.i("Noise", "==== start ===");
+        mSensor.start();
+        if (!mWakeLock.isHeld()) {
+            mWakeLock.acquire();
+        }
+        //Noise monitoring start
+        // Runnable(mPollTask) will execute after POLL_INTERVAL
+        mHandler.postDelayed(mPollTask, POLL_INTERVAL);
+    }
+    private void stop() {
+        Log.d("Noise", "==== Stop Noise Monitoring===");
+        if (mWakeLock.isHeld()) {
+            mWakeLock.release();
+        }
+        mHandler.removeCallbacks(mSleepTask);
+        mHandler.removeCallbacks(mPollTask);
+        mSensor.stop();
+        bar.setProgress(0);
+        updateDisplay("stopped...", 0.0);
+        mRunning = false;
+    }
+
+    private void initializeApplicationConstants() {
+        // Set Noise Threshold
+        mThreshold = progress_value;
+
+
+    }
+
+    private void updateDisplay(String status, double signalEMA) {
+        mStatusView.setText(status);
+        bar.setProgress((int)signalEMA);
+        Log.d("SONUND", String.valueOf(signalEMA));
+        tv_noice.setText(signalEMA+"dB");
+
+    }
+
+    private void callForHelp(double signalEMA) {
+
+        //stop();
+        // Show alert when noise thersold crossed
+        switchFlashLight(true);
+        //Toast.makeText(getApplicationContext(), "Noise Thersold Crossed, do here your stuff.",Toast.LENGTH_LONG).show();
+
+        Log.d("SONUND", String.valueOf(signalEMA));
+        tv_noice.setText(signalEMA+"dB");
+    }
+    public void switchFlashLight(boolean status) {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, status);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+};
